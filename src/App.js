14d750458124ae2a8b9b9d73c95192cb6647d692
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-18 07:12:32 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
