0e1cfad641ccd459e9272f12a6d7b91174a88792
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-21 06:45:02 +0100

    Movie page

--- src/views/MoviePage.js
+++ src/views/MoviePage.js
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { Wrapper } from "./MainPage";
-import Scroll from "../components/Scroll";
+import { Wrapper } from "../views/MainPage";
+import { connect } from "react-redux";
+import { fetchPopularMovies, fetchMoviesByKeyword } from "../actions";
+
 import MovieList from "../components/MovieList";
 
-export default class SidebarNews extends Component {
+class MoviePage extends Component {
+  componentDidMount() {
+    this.props.fetchPopularMovies();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.keyword !== this.props.keyword) {
+      this.props.fetchMoviesByKeyword(this.props.keyword);
+    }
+  }
+
   render() {
+    const { popularMovies } = this.props;
+    console.log("popularMovies", popularMovies);
+
+    const isMovieBykeyword = this.props.moviesByKeyword.length > 0;
+
     return (
       <Wrapper>
-        <Scroll>
-          <MovieList />;
-        </Scroll>
+        <MovieList
+          movies={isMovieBykeyword ? this.props.moviesByKeyword : popularMovies}
+        />
       </Wrapper>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    popularMovies: state.popularMovies,
+    moviesByKeyword: state.moviesByKeyword,
+    keyword: state.keyword,
+  };
+};
+
+export default connect(mapStateToProps, {
+  fetchPopularMovies,
+  fetchMoviesByKeyword,
+})(MoviePage);
