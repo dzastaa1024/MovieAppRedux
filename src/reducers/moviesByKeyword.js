9ab2d155bb51fa21075a0009c10f677b9eaa4780
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-22 14:55:00 +0100

    cleaning

--- src/reducers/moviesByKeyword.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-  switch (action.type) {
-    case "FETCH_MoviesByKeyword":
-      return action.payload;
-    default:
-      return state;
-  }
-};
--- src/reducers/topratedReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-  switch (action.type) {
-    case "FETCH_TopRatedMovies":
-      return action.payload;
-    default:
-      return state;
-  }
-};
