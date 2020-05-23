763547feebd6269174cb00115f738b39ddce3196
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-23 12:05:49 +0100

    implement localStorage

---
+++ src/localStorage/localStorage.js
@@ -0,0 +1,17 @@
+export const loadLocalStorage = (key) => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    if (serializedState === null) return undefined;
+
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+export const setLocalStorage = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (e) {}
+};
