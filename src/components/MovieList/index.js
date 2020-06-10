2c108034ab7abcf6530de0235716c6638772a4f7
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-06-10 16:46:57 +0100

    refactoring

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,52 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import { Wrapper, List, Slider, Title } from "./style";
 
 import MovieItem from "../MovieItem";
 
-class MovieList extends Component {
-  render() {
-    const { movies, sidebarNews } = this.props;
-
-    if (Array.isArray(movies)) {
-      return (
+const MovieList = ({ movies, sidebarNews }) => {
+  if (Array.isArray(movies)) {
+    return (
+      <Wrapper>
+        <Title sidebarNews={sidebarNews}>Results</Title>
+        <List sidebarNews={sidebarNews}>
+          {movies.map((movie) => {
+            return (
+              <MovieItem
+                movie={movie}
+                key={movie.id}
+                sidebarNews={sidebarNews}
+              />
+            );
+          })}
+        </List>
+      </Wrapper>
+    );
+  } else {
+    const { topRatedMovies, popularMovies } = movies;
+    return (
+      <>
         <Wrapper>
-          <Title sidebarNews={sidebarNews}>Results</Title>
-          <List sidebarNews={sidebarNews}>
-            {movies.map((movie) => {
-              return (
-                <MovieItem
-                  movie={movie}
-                  key={movie.id}
-                  sidebarNews={sidebarNews}
-                />
-              );
+          <Title>Top rated movies</Title>
+          <Slider>
+            {topRatedMovies.map((movie) => {
+              return <MovieItem movie={movie} key={movie.id} />;
             })}
-          </List>
+          </Slider>
+          <Title>Popular movies</Title>
+          <Slider>
+            {popularMovies.map((movie) => {
+              return <MovieItem movie={movie} key={movie.id} />;
+            })}
+          </Slider>
         </Wrapper>
-      );
-    } else {
-      const { topRatedMovies, popularMovies } = movies;
-
-      return (
-        <>
-          <Wrapper>
-            <Title>Top rated movies</Title>
-            <Slider>
-              {topRatedMovies.map((movie) => {
-                return <MovieItem movie={movie} key={movie.id} />;
-              })}
-            </Slider>
-            <Title>Popular movies</Title>
-            <Slider>
-              {popularMovies.map((movie) => {
-                return <MovieItem movie={movie} key={movie.id} />;
-              })}
-            </Slider>
-          </Wrapper>
-        </>
-      );
-    }
+      </>
+    );
   }
-}
+};
 
 export default MovieList;
