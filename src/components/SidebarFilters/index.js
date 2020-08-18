fd27cd1e5aa18bf28f7177094248f10d9ddc8bc4
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-08-18 21:42:43 +0100

    refacoring

--- src/components/SidebarFilters/index.js
+++ src/components/SidebarFilters/index.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import { Wrapper } from "./style";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { fetchGenre, fetchLanguages } from "../../actions/API_ACTION";
 
 import SideBarFiltersList from "../SideBarFiltersList";
 
-const SidebarFilters = ({ genres, languages, fetchGenre, fetchLanguages }) => {
+const SidebarFilters = () => {
+  const genres = useSelector(state => state.dataApi.genres);
+  const languages = useSelector(state => state.dataApi.languages);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    fetchGenre();
-    fetchLanguages();
+    dispatch(fetchGenre());
+    dispatch(fetchLanguages());
   }, []);
 
   // componentDidMount() {
@@ -32,13 +36,6 @@ const SidebarFilters = ({ genres, languages, fetchGenre, fetchLanguages }) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    genres: state.dataApi.genres,
-    languages: state.dataApi.languages,
-  };
-};
 
-export default connect(mapStateToProps, { fetchGenre, fetchLanguages })(
-  SidebarFilters
-);
+export default SidebarFilters;
+
